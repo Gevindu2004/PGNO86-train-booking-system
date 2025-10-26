d0061c7ee532fdb41a57f31bb958aa23bda048c0
Gevindu2004 <gevindujayarathne@gmail.com>
2025-10-26 14:39:05 +0530

    Delete src directory

--- src/main/java/com/trainmanagement/trainmanagementsystem/repository/TrainRepository.java
+++
@@ -1,8 +0,0 @@
-package com.trainmanagement.trainmanagementsystem.repository;
-
-
-import com.trainmanagement.trainmanagementsystem.entity.Train;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrainRepository extends JpaRepository<Train, Long> {
-}
